9ee8d5c0da9f080617a6dc60b18f73717b05d869
phiunit <26776324+phiunit@users.noreply.github.com>
2023-04-05 16:55:43 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+var paddle1 = document.getElementById("paddle1");
+var paddle2 = document.getElementById("paddle2");
+var ball = document.getElementById("ball");
+
+paddle1.addEventListener("click", function() {
+  this.style.transform = "translate(0, -10px
